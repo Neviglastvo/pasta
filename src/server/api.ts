36a7b40aff2018054eca90c99762e6f06f9c6b48
neviglastvo <ParadiseSeeker94@Gmail.com>
2024-04-02 02:41:04 +0300

    adding postgres

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,6 +1,7 @@
 import { remultExpress } from "remult/remult-express"
 import { QuoteEntity } from "../shared/QuoteEntity"
 
+
 export const api = remultExpress({
   entities: [QuoteEntity]
 })
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,15 @@
 import express from "express"
 import { api } from "./api"
-import { JsonDataProvider } from "remult";
-import { JsonEntityFileStorage } from "remult/server";
 import { remultExpress } from "remult/remult-express";
 import bodyParser from "body-parser";
+import helmet from "helmet";
+import compression from "compression";
+import path from "path"
+import { createPostgresDataProvider } from "remult/postgres"
 
-// Define the whitelist of IP addresses
-const whitelist = ['::1','192.168.1.1', '127.0.0.1']; // Add your whitelisted IPs here
+const PORT = process.env["PORT"] || 3001
+const DATABASE_URL = process.env["DATABASE_URL"];
+const IP_WHITELIST = process.env["IP_WHITELIST"] || ['::1'];
 
 const app = express()
 
@@ -19,23 +22,29 @@ app.use((req, res, next) => {
   const clientIp = req.ip;
   console.log("clientIp", clientIp);
 
-  if (!whitelist.includes(clientIp)) {
+  if (!IP_WHITELIST.includes(clientIp)) {
     return res.status(403).send('Access Forbidden');
   }
 
   next();
 });
 
+app.use(helmet())
+app.use(compression())
+
 app.use(
   remultExpress({
-    dataProvider: async () =>
-      new JsonDataProvider(new JsonEntityFileStorage("./db"))
+    dataProvider: DATABASE_URL
+   ? createPostgresDataProvider({ connectionString: DATABASE_URL })
+   : undefined,
   })
 )
 
 app.use(api)
 
+app.use(express.static(path.join(__dirname, "../")))
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(__dirname, "../", "index.html"))
+})
 
-
-app.get("*", (req, res) => res.send(`api Server - path: "${req.path}"`))
-app.listen(3001, () => console.log(`Server started http://localhost:${3001}/api`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
